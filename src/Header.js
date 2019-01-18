47bb3e5324f2acfed15bbd499cc9496d76e8b0c8
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-18 22:20:42 +0000

    Added title to <Header/>

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div></div>
+      <div className="header">
+        <h1 className="logo">Gallery App</h1>
+      </div>
     )
   }
 }
