bf13f7ec96710d7fc1f3af964d4113caf53167dd
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-19 10:59:49 +0000

    Added <Gallery/> HTML

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class Gallery extends Component {
   render() {
     return (
-      <div></div>
+      <div className="gallery">
+        <h2 className="gallery-title">Title</h2>
+
+        <div className="gallery-paintings">
+          <ol className="paintings-grid">
+          </ol>
+        </div>
+      </div>
     )
   }
 }
