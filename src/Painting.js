dfc413f75801a492bd8632f1f435160d6158f4ec
StevenRJClarke1985 <srjc3@cantab.net>
2019-03-09 18:50:44 +0000

    <Painting/>s display an image and are formatted

--- src/Painting.js
+++ src/Painting.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 class Painting extends Component {
   render() {
     return (
-      <div></div>
+      <li>
+        <div className="painting">
+          <img
+            className="painting-image"
+            src={
+              (this.props.painting.imageURL)
+              ? this.props.painting.imageURL
+              : "http://via.placeholder.com/128x193?text=?"
+            }
+          />
+        </div>
+      </li>
     )
   }
 }
