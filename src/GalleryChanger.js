731b0c34ce96e106a093198d3d8dfb156a18f3cf
StevenRJClarke1985 <srjc3@cantab.net>
2019-03-10 15:47:41 +0000

    <GalleryChanger/> given a <select> element to choose gallery

--- src/GalleryChanger.js
+++ src/GalleryChanger.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class GalleryChanger extends Component {
   render () {
     return (
-      <div></div>
+      <div className="gallery-changer">
+        <select>
+          <option value="move" disabled>Move to...</option>
+          <option value="seen">Seen</option>
+          <option value="want-to-see">Want to See</option>
+          <option value="not-seen">Not Seen</option>
+        </select>
+      </div>
     )
   }
 }
